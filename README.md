1f08e0b00263b6f089b1b19e591028863f030e23
Alpha <robbiew@gmail.com>
2022-02-22 08:25:23 -0800

    Update README

---
+++ README.md
@@ -0,0 +1,105 @@
+# godoors
+
+Helpful BBS "door" library in GO for creating linux-based applications that run over STDIO.
+
+## Install
+
+```go get github.com/robbiew/godoors```
+
+## General functions
+
+```go 
+godoors.DropFileData(path string) (string, int, int)
+
+// Pass the path of the Door32.sys BBS drop file (including trailing slash), and itll return HANDLE/ALIAS, TIME LEFT (in minutes) and EMULATION TYPE (0 = Ascii, 1 = Ansi). You'll probably want to do this using a "startDoor.sh" file or from your BBS menu command (e.g. /path/to/drop/$NODE/Door32.sys/).
+```
+
+```go
+godoors.GetTermSize() (int, int)
+
+// Tries to detect the user's terminal size. Returns HEIGHT and WIDTH. If it can't detect, it'll default to 25 and 80.
+```
+
+```go
+godoors.PrintAnsi(file string)
+
+// Pass the path of a valid ANSI art file and it'll strip the SAUCE record, then print line by line.
+```
+
+```go
+godoors.PrintAnsiLoc(file string, x int, y int)
+
+// Same as above, only it'll print the art to the screen at X/Y coordinates, incrementing the Y position after every line.
+```
+## Cursor related
+```go
+// Move the cursor n cells to up.
+func CursorUp(n int) 
+
+// Move the cursor n cells to down.
+func CursorDown(n int) 
+
+// Move the cursor n cells to right.
+func CursorForward(n int) 
+
+// Move the cursor n cells to left.
+func CursorBack(n int) 
+
+// Move cursor to beginning of the line n lines down.
+func CursorNextLine(n int) 
+
+// Move cursor to beginning of the line n lines up.
+func CursorPreviousLine(n int) 
+
+// Move cursor horizontally to x.
+func CursorHorizontalAbsolute(x int) 
+
+// Show the cursor.
+func CursorShow() 
+
+// Hide the cursor.
+func CursorHide()
+```
+
+## Color
+```go
+// Text color  -- e.g. fmt.Println(godoors.Yellow)
+Black         
+Red          
+Green         
+Yellow     
+Blue        
+Magenta      
+Cyan         
+White         
+BrightBlack   
+BrightRed    
+BrightGreen   
+BrightYellow  
+BrightBlue    
+BrightMagenta 
+BrightCyan    
+BrightWhite   
+
+// Background colors
+BgBlack        
+BgRed          
+BgGreen        
+BgYellow       
+BgBlue          
+BgMagenta       
+BgCyan          
+BgWhite         
+BgBrightBlack   
+BgBrightRed     
+BgBrightGreen   
+BgBrightYellow  
+BgBrightBlue    
+BgBrightMagenta 
+BgBrightCyan    
+BgBrightWhite   
+
+// Reset to default colors
+ColorReset 
+```
+See ```godoors.go``` for other misc. functions.
--- goodoors.go
+++ goodoors.go
@@ -13,13 +13,137 @@ import (
 	"unicode/utf8"
 )
 
-func DropFileData(nodeNum string, path string) (string, int, int) {
+// CREDIT TO https://github.com/k0kubun/go-ansi for some of these sequences,
+
+// Common fragments of escape sequences
+const (
+	Esc = "\u001B["
+	Osc = "\u001B]"
+	Bel = "\u0007"
+)
+
+// Common ANSI escapes sequences. These should be used when the desired action
+// is only needed once; otherwise, use the functions (e.g. moving a cursor
+// several lines/columns). See: https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
+const (
+	CursorBackward = Esc + "D"
+	CursorPrevLine = Esc + "F"
+	CursorLeft     = Esc + "G"
+	CursorTop      = Esc + "d"
+	CursorTopLeft  = Esc + "H"
+
+	CursorBlinkEnable  = Esc + "?12h"
+	CursorBlinkDisable = Esc + "?12I"
+
+	ScrollUp   = Esc + "S"
+	ScrollDown = Esc + "T"
+
+	TextInsertChar = Esc + "@"
+	TextDeleteChar = Esc + "P"
+	TextEraseChar  = Esc + "X"
+	TextInsertLine = Esc + "L"
+	TextDeleteLine = Esc + "M"
+
+	EraseRight  = Esc + "K"
+	EraseLeft   = Esc + "1K"
+	EraseLine   = Esc + "2K"
+	EraseDown   = Esc + "J"
+	EraseUp     = Esc + "1J"
+	EraseScreen = Esc + "2J"
+
+	TextColorBlack         = Esc + "30m"
+	TextColorRed           = Esc + "31m"
+	TextColorGreen         = Esc + "32m"
+	TextColorYellow        = Esc + "33m"
+	TextColorBlue          = Esc + "34m"
+	TextColorMagenta       = Esc + "35m"
+	TextColorCyan          = Esc + "36m"
+	TextColorWhite         = Esc + "37m"
+	TextColorBrightBlack   = Esc + "30;1m"
+	TextColorBrightRed     = Esc + "31;1m"
+	TextColorBrightGreen   = Esc + "32;1m"
+	TextColorBrightYellow  = Esc + "33;1m"
+	TextColorBrightBlue    = Esc + "34;1m"
+	TextColorBrightMagenta = Esc + "35;1m"
+	TextColorBrightCyan    = Esc + "36;1m"
+	TextColorBrightWhite   = Esc + "37;1m"
+
+	BackgroundColorBlack         = Esc + "40m"
+	BackgroundColorRed           = Esc + "41m"
+	BackgroundColorGreen         = Esc + "42m"
+	BackgroundColorYellow        = Esc + "43m"
+	BackgroundColorBlue          = Esc + "44m"
+	BackgroundColorMagenta       = Esc + "45m"
+	BackgroundColorCyan          = Esc + "46m"
+	BackgroundColorWhite         = Esc + "47m"
+	BackgroundColorBrightBlack   = Esc + "40;1m"
+	BackgroundColorBrightRed     = Esc + "41;1m"
+	BackgroundColorBrightGreen   = Esc + "42;1m"
+	BackgroundColorBrightYellow  = Esc + "43;1m"
+	BackgroundColorBrightBlue    = Esc + "44;1m"
+	BackgroundColorBrightMagenta = Esc + "45;1m"
+	BackgroundColorBrightCyan    = Esc + "46;1m"
+	BackgroundColorBrightWhite   = Esc + "47;1m"
+
+	ColorReset = Esc + "0m"
+
+	ClearScreen = "\u001Bc"
+)
+
+// Move the cursor n cells to up.
+func CursorUp(n int) {
+	fmt.Printf("\x1b[%dA", n)
+}
+
+// Move the cursor n cells to down.
+func CursorDown(n int) {
+	fmt.Printf("\x1b[%dB", n)
+}
+
+// Move the cursor n cells to right.
+func CursorForward(n int) {
+	fmt.Printf("\x1b[%dC", n)
+}
+
+// Move the cursor n cells to left.
+func CursorBack(n int) {
+	fmt.Printf("\x1b[%dD", n)
+}
+
+// Move cursor to beginning of the line n lines down.
+func CursorNextLine(n int) {
+	fmt.Printf("\x1b[%dE", n)
+}
+
+// Move cursor to beginning of the line n lines up.
+func CursorPreviousLine(n int) {
+	fmt.Printf("\x1b[%dF", n)
+}
+
+// Move cursor horizontally to x.
+func CursorHorizontalAbsolute(x int) {
+	fmt.Printf("\x1b[%dG", x)
+}
+
+// Show the cursor.
+func CursorShow() {
+	fmt.Print("\x1b[?25h")
+}
+
+// Hide the cursor.
+func CursorHide() {
+	fmt.Print("\x1b[?25l")
+}
+
+func DropFileData(path string) (string, int, int) {
+
+	// path needs to include trailing slash
 
 	var dropAlias string
 	var dropTimeLeft string
 	var dropEmulation string
 
-	file, err := os.Open(strings.ToLower(path + "/" + nodeNum + "/" + "door32.sys"))
+	file, err := os.Open(strings.ToLower(path + "door32.sys"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -79,6 +203,7 @@ func GetTermSize() (int, int) {
 	*/
 
 	// Set the terminal to raw mode so we aren't waiting for CLRF rom user (to be undone with `-raw`)
+
 	rawMode := exec.Command("/bin/stty", "raw")
 	rawMode.Stdin = os.Stdin
 	_ = rawMode.Run()
@@ -117,20 +242,20 @@ func GetTermSize() (int, int) {
 		}
 		h := ih
 		w := iw
+
 		return h, w
 
 	} else {
 		// couldn't detect, so let's just set 80 x 25 to be safe
 		h := 80
 		w := 25
+
 		return h, w
 	}
 
 }
 
-func PrintAnsi(file string, h int, w int) {
-
-	rowCount := 0
+func PrintAnsi(file string) {
 
 	content, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -141,9 +266,7 @@ func PrintAnsi(file string, h int, w int) {
 	s := bufio.NewScanner(strings.NewReader(string(noSauce)))
 
 	for s.Scan() {
-
 		fmt.Println(s.Text())
-		rowCount++
 	}
 
 }
@@ -173,4 +296,24 @@ func TrimLastChar(s string) string {
 		size = 0
 	}
 	return s[:len(s)-size]
+}
+
+func PrintAnsiLoc(file string, x int, y int) {
+
+	yLoc := y
+
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	noSauce := TrimStringFromSauce(string(content)) // strip off the SAUCE metadata
+	s := bufio.NewScanner(strings.NewReader(string(noSauce)))
+
+	for s.Scan() {
+		fmt.Println("\x1b[" + strconv.Itoa(yLoc) + ";" + strconv.Itoa(x) + "f")
+		fmt.Println(s.Text())
+		yLoc++
+	}
+
 }
